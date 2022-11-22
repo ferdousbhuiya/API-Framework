052c91b05003e9126fcc9b62bf0af7489617858c
ferdousbhuiya <ferdousbhuiya@gmail.com>
2022-11-21 22:15:55 -0500

    API-Framwork

---
+++ src/test/java/Request/RequestFactory.java
@@ -0,0 +1,26 @@
+package Request;
+
+import Client.RestClient;
+import io.restassured.response.Response;
+
+public class RequestFactory {
+    RestClient restClient;
+    public RequestFactory()
+    {
+        restClient = new RestClient();
+    }
+
+    public Response getAllProduct()
+    {
+        return restClient.SendGetRequest("/products");
+    }
+
+    public Response addProduct( Object requestPayload)
+    {
+        return restClient.SendPostRequest("/products", requestPayload);
+    }
+    public Response updateProduct( Object requestPayload)
+    {
+        return restClient.SendPutRequest("/products", requestPayload);
+    }
+}
---
+++ src/test/java/Tests/ProductTests.java
@@ -0,0 +1,99 @@
+package Tests;
+
+import com.aventstack.extentreports.Status;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProductTests extends BaseTest{
+
+    @Test
+    public void verifyGetProduct()
+    {
+        extentReport.createTestCase("Verify Get Product");
+        Response response = requestFactory.getAllProduct();
+        extentReport.addLog(Status.INFO, response.asPrettyString());
+        response.then().statusCode(200);
+    }
+    @Test
+    public void verifyAddProduct()
+    {
+        extentReport.createTestCase("Verify Add Product");
+        String requestPayload= "{\n" +
+                "   \"name\":\"iPhone 14\",\n" +
+                "   \"type\":\"Mobile Phone\",\n" +
+                "   \"price\":1000,\n" +
+                "   \"shipping\":0,\n" +
+                "   \"upc\":\"string\",\n" +
+                "   \"description\":\"string\",\n" +
+                "   \"manufacturer\":\"string\",\n" +
+                "   \"model\":\"string\",\n" +
+                "   \"url\":\"string\",\n" +
+                "   \"image\":\"string\"\n" +
+                "}";
+        requestFactory.addProduct(requestPayload).then().log().all().statusCode(201);
+
+
+    }
+
+    @Test
+    public void verifyAddProductWithRequestsPayloadMap()
+    {
+        extentReport.createTestCase("Verify Add product with request payload as map");
+        Map<String, Object > requestPayload = new HashMap<>();
+        requestPayload.put("name", "iPhone14");
+        requestPayload.put("type", "Mobile phone");
+        requestPayload.put("price", 1000);
+        requestPayload.put("shipping", 10);
+        requestPayload.put("upc", "iPhone14");
+        requestPayload.put("description", "iPhone14");
+        requestPayload.put("manufacturer", "Apple");
+        requestPayload.put("model", "iPhone14");
+        requestPayload.put("url", "iPhone14");
+        requestPayload.put("image", "iPhone14");
+
+        requestFactory.addProduct(requestPayload).then().log().all().statusCode(201);
+
+
+    }
+
+    @Test
+    public void verifyEndToEndCallFlow()
+    {
+
+        // 1. Send a Post Request to Create a product
+
+        Map<String, Object > requestPayload = new HashMap<>();
+        requestPayload.put("name", "iPhone14");
+        requestPayload.put("type", "Mobile phone");
+        requestPayload.put("price", 1000);
+        requestPayload.put("shipping", 10);
+        requestPayload.put("upc", "iPhone14");
+        requestPayload.put("description", "iPhone14");
+        requestPayload.put("manufacturer", "Apple");
+        requestPayload.put("model", "iPhone14");
+        requestPayload.put("url", "iPhone14");
+        requestPayload.put("image", "iPhone14");
+
+        Response response = requestFactory.addProduct(requestPayload);
+        response.then().log().all().statusCode(201);
+
+
+
+
+        // Get ID of the product.
+
+        // 2. Send a Get Request to verify if the product is added.
+
+        // 3. Send a Put request to edit the porduct.
+
+        // 4. Send a Delete request to delete the product.
+
+        // 5 . Send a Get request to verify the delete.
+
+
+
+    }
+}
